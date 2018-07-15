85f9a26be43836961af0e2cd6200ca78e112027b
loveai.info <loveai.info@gmail.com>
2018-07-15 11:40:28 +0800

    delete elfviewer

--- ELFViewer/app/src/main/cpp/utils/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_COMMON_H
-#define UTILS_COMMON_H
-
-#include "config.h"
-#include "log.h"
-#include "debug.h"
-
-#endif
--- ELFViewer/app/src/main/java/info/loveai/jni/Native.java
+++
@@ -1,9 +0,0 @@
-package info.loveai.jni;
-
-public final class Native {
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-    public static native String stringFromJNI();
-}
